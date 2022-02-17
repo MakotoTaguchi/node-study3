bc400889770a543faf7ac5c3d4d62828e247711a
MakotoTaguchi <taguchimakoto@icloud.com>
2022-02-17 21:11:16 +0900

    main

--- routes/hello.js
+++ routes/hello.js
@@ -8,9 +8,9 @@ const db = new sqlite3.Database('mydb.sqlite3');
 router.get('/', (req, res, next) => {
     db.serialize(() => {
         var rows = "";
-        db.each("select * from mydata", (err, rows) => {
+        db.each("select * from mydata", (err, row) => {
             if (!err) {
-                rows += "<tr><th>" + rows.id + "</th></td>" + rows.name + "</td><td></tr>";
+                rows += "<tr><th>" + row.id + "</th></td>" + row.name + "</td><td></tr>";
             }
         }, (err, count) => {
             if (!err) {
