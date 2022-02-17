51b64a3e35a292bfae396c35fd6cdc94974cc6a6
MakotoTaguchi <taguchimakoto@icloud.com>
2022-02-17 23:42:22 +0900

    main

--- routes/hello.js
+++ routes/hello.js
@@ -18,10 +18,28 @@ router.get('/', (req, res, next) => {
                     title: 'Hello!',
                     content: rows
                 };
-                res.render('hello', data);
+                res.render('hello/index', data);
             }
         });
     });
 });
+
+router.get('/add', (req, res, next) => {
+    var data = {
+        title: 'Hello/Add',
+        content: '新しいレコードを入力：'
+    }
+    res.render('hello/add', data);
+});
+
+router.post('/add', (req, res, next) => {
+    const nm = req.body.name;
+    const ml = req.body.mail;
+    const ag = req.body.age;
+    db.serialize(() => {
+        db.run('insert into mydata (name, mail, age) values (?, ?, ?)', nm, ml, ag);
+    });
+    res.redirect('/hello');
+})
 
 module.exports = router;
