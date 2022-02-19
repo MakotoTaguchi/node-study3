f2ff22426987034bc6ab0c9d5f69a508c94771bc
MakotoTaguchi <taguchimakoto@icloud.com>
2022-02-19 20:21:35 +0900

    main

--- routes/users.js
+++ routes/users.js
@@ -2,35 +2,65 @@ var express = require('express');
 var router = express.Router();
 const db = require('../models/index');
 
-const {
-  Op
-} = require('sequelize');
+// const {
+//   Op
+// } = require('sequelize');
 
-/* GET users listing. */
+// /* GET users listing. */
+// router.get('/', (req, res, next) => {
+//   const nm = req.query.name;
+//   const ml = req.query.mail;
+//   db.User.findAll({
+//     where: {
+//       [Op.or]: [{
+//           name: {
+//             [Op.like]: '%' + nm + '%'
+//           }
+//         },
+//         {
+//           mail: {
+//             [Op.like]: '%' + ml + '%'
+//           }
+//         }
+//       ]
+//     }
+//   }).then(usrs => {
+//     var data = {
+//       title: 'User/Index',
+//       content: usrs
+//     }
+//     res.render('users/index', data);
+//   });
+// });
+
 router.get('/', (req, res, next) => {
-  const nm = req.query.name;
-  const ml = req.query.mail;
-  db.User.findAll({
-    where: {
-      [Op.or]: [{
-          name: {
-            [Op.like]: '%' + nm + '%'
-          }
-        },
-        {
-          mail: {
-            [Op.like]: '%' + ml + '%'
-          }
-        }
-      ]
-    }
-  }).then(usrs => {
+  db.User.findAll().then(usrs => {
     var data = {
-      title: 'User/Index',
+      title: 'Users/Index',
       content: usrs
     }
     res.render('users/index', data);
   });
+});
+
+router.get('/add', (req, res, next) => {
+  var data = {
+    title: 'User/add'
+  }
+  res.render('users/add', data);
+});
+
+router.post('/add', (req, res, next) => {
+  db.sequelize.sync()
+    .then(() => db.User.create({
+      name: req.body.name,
+      pass: req.body.pass,
+      mail: req.body.mail,
+      age: req.body.age
+    }))
+    .then(usr => {
+      res.redirect('/users');
+    });
 });
 
 module.exports = router;
