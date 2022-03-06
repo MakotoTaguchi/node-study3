6b5ae6c9d0cb407670ac1e7bb046a2b3ba8830f7
MakotoTaguchi <taguchimakoto@icloud.com>
2022-03-06 17:47:06 +0900

    main

--- routes/users.js
+++ routes/users.js
@@ -75,13 +75,30 @@ router.get('/edit', (req, res, next) => {
 });
 
 router.post('/edit', (req, res, next) => {
+  db.User.findByPk(req.body.id)
+    .then(usr => {
+      usr.name = req.body.name;
+      usr.pass = req.body.pass;
+      usr.mail = req.body.mail;
+      usr.age = req.body.age;
+      usr.save().then(() => res.redirect('/users'));
+    });
+});
+
+router.get('/delete', (req, res, next) => {
+  db.User.findByPk(req.query.id)
+    .then(usr => {
+      var data = {
+        title: 'Users/Delete',
+        form: usr
+      }
+      res.render('users/delete', data);
+    });
+});
+
+router.post('/delete', (req, res, next) => {
   db.sequelize.sync()
-    .then(() => db.User.update({
-      name: req.body.name,
-      pass: req.body.pass,
-      mail: req.body.mail,
-      age: req.body.age
-    }, {
+    .then(() => db.User.destroy({
       where: {
         id: req.body.id
       }
