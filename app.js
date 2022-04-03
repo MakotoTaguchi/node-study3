7a8cd9ae766294534057040fbc0bb74d4de6fb75
MakotoTaguchi <taguchimakoto@icloud.com>
2022-04-03 20:06:20 +0900

    main

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var boardsRouter = require('./routes/boards');
 var hello = require('./routes/hello');
 
 var app = express();
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', hello);
+app.use('/boards', boardsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
